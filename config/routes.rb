7727564078d4a8e382b989a777fd2a2d7a1c66ed
adwaith-os <adwi@adwi.nitk.ac.in>
2018-10-23 14:26:22 +0530

    Added basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'welcome/index'
   root 'welcome#index'
 
   get '/what_is_a_ctf',     to: 'welcome#what_is_a_ctf'
   get '/about',             to: 'welcome#about'
   get '/signup',            to: 'users#new'
-  post '/signup',            to: 'users#create'
-  resources :users           
+  post '/signup',           to: 'users#create'
+  get '/login',             to: 'sessions#new'
+  post '/login',            to: 'sessions#create'
+  delete '/logout',         to: 'sessions#destroy'
+  resources :users
+  get '/questions',         to: 'questions#index'
+  get '/questions',         to: 'questions#new'
+           
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
